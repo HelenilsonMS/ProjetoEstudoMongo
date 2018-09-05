fb0088f166a017048fa6cf612de596e9e45dd45b
Helenilson Matias <Helenilson.soares@tcs.com>
2018-09-05 11:38:45 -0500

    primeiro commit

---
+++ src/main/java/com/example/demo/ProjetoEstudoMongoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudoMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudoMongoApplication.class, args);
+	}
+}
